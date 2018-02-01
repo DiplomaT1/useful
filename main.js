c41dde4e802d44bb5d51339f0fdf6a1f0b2c7dea
Diplomat1 <crywolfie93@gmail.com>
2018-02-01 21:09:35 +0200

    slidersmulti

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// multiple sliders
+
+$('.slider').each(function(){
+var slickInduvidual = $(this);
+slickInduvidual.slick({
+nextArrow: slickInduvidual.next().find('.right'),
+prevArrow: slickInduvidual.next().find('.left')
+});
+})
