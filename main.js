ce2d37b962c0ef132a576a3ff6f94211de9da741
dmitriy <dmitriy@coderiver.com.ua>
2018-03-19 15:34:04 +0200

    math random

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ prevArrow: slickInduvidual.next().find('.left')
 })
 
 console.log('try');
+
+
+$(document).ready(function () {
+    var x = document.getElementsByClassName('over')[0];
+
+    $(x).on('mouseover', function () {
+        console.log(x);
+        var min = 0, max = 98;
+        function randomInteger(min, max) {
+            var rand = min - 0.5 + Math.random() * (max - min + 1)
+            rand = Math.round(rand);
+            return rand;
+        }
+        
+        $(x).css('left', randomInteger(min, max) + '%');
+        $(x).css('top', randomInteger(min, max) + '%');
+        console.log(randomInteger(min, max));
+    });
+
+
+});
